5eb5338fcc412e7f98335efa3fcf6fb06cfbadc0
marisofip <marisofipalencia@gmail.com>
2023-03-19 01:45:48 +0000

    Updates

--- js/views/estudios.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../../styles/home.css";
-import estudios from "../../../src/img/estudios.png";
-
-
-export const Study = () => (
-	< div className="container" id="exp">
-	          <img className="rounded mx-auto d-block img-fluid" src={estudios} style={{borderRadius:"70%"}}/>      
-	</div>)
